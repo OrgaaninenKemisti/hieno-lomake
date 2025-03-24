087e12e16bf41db6f0cc1c5a160413768418148d
Elias Hawkins <Elias.Hawkins@edu.mayk.fi>
2025-03-24 14:53:26 +0200

    y

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from wsgiref.simple_server import make_server
 
 app = Flask(__name__)
 
@@ -10,5 +11,17 @@ def root():
 def vastaus():
     return render_template('vastaus.html', nimi=request.args['nimi'])
 
+@app.route("/bigwin")
+def bigwin():
+    return render_template('bigwin.html', massi=request.args['massi'])
+@app.route("/cheeky")
+def cheeky():
+    return render_template('cheeky.html')
+
 if __name__ == "__main__":
-    app.run()
+    #app.run()
+    try: 
+        with make_server("localhost", 8000, app) as server: 
+            server.serve_forever()
+    except KeyboardInterrupt:
+        print("hei hei")
